03cb63cd3b86785e4210c2eee7dd775ab0817aae
Turchetto <diegoturchetto@gmail.com>
2019-11-20 21:18:18 -0200

    T

--- src/jogovelha/JogoVelha.java
+++ src/jogovelha/JogoVelha.java
@@ -14,7 +14,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.*;
 
